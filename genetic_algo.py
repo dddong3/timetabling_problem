db0e44a2e4cda8bd287ff1c156e937a5db70e582
dong3 <dong3@dong3deMacBook-Pro.local>
2023-10-05 00:25:20 +0800

    remove private member

--- genetic_algo.py
+++ genetic_algo.py
@@ -1,32 +1,47 @@
 import time
 import json
 import random
+from chromosome_fitness import Fitness
 
 class GeniticAlgoithm:
-    def __init__(self: object, courseFileName: str, classFileName: str, *, popu: int, live: int) -> None:
-        self.__START_WEEK, self.__END_WEEK = 1, 5
-        assert(self.__START_WEEK <= self.__END_WEEK)
-        #                      0      1     2     3     4     5     6     7     8     9     10    11    12    13    14
-        self.__session_list = ['01', '02', '03', '04', '20', '05', '06', '07', '08', '09', '40', '50', '60', '70']
-        self.__gene_list = self.get_json(courseFileName)
-        self.__class_detail = self.get_json(classFileName)
-        self.__class_list = list(self.__class_detail.keys())
-        self.__population_size = popu
-        self.__livecycle = live
+    START_WEEK, END_WEEK = 1, 5
+    session_list = ['01', '02', '03', '04', '20', '05', '06', '07', '08', '09', '40', '50', '60', '70']
+    livecycle = None
+    gene_list = None
+    class_list = None
+    class_detail = None
+    population_size = None
+    def __init__(self: object) -> None:
+        assert(self.START_WEEK <= self.END_WEEK)
+        assert(self.population_size is not None)
+        assert(self.livecycle is not None)
+        assert(self.gene_list is not None)
+        assert(self.class_detail is not None)
+        assert(self.class_list is not None)
+
         self.best_fitness = None
         self.best_chromosome = None
         # self.__fitness = [self.cal_fitness(chromosome) for chromosome in self.__chromosome]
         self.run()
         # print(self.best_fitness)
 
+    @classmethod
+    def init(cls, courseFileName: str, classFileName: str, *, popu: int, live: int) -> None:
+        cls.population_size = popu
+        cls.livecycle = live
+        cls.gene_list = GeniticAlgoithm.get_json(courseFileName)
+        cls.class_detail = GeniticAlgoithm.get_json(classFileName)
+        cls.class_list = list(cls.class_detail.keys())
+
+
     def __lt__(self: object, other: object) -> bool:
         return self.best_fitness < other.best_fitness
 
     def run(self: object) -> None:
-        self.__chromosome = [self.generate_chromosome() for _ in range(self.__population_size)]
-        self.__fitness = [self.cal_fitness(chromosome) for chromosome in self.__chromosome]
+        self.chromosome = [self.generate_chromosome() for _ in range(self.population_size)]
+        self.fitness = [self.cal_fitness(chromosome) for chromosome in self.chromosome]
         self.update_best_fitness()
-        for _ in range(self.__livecycle):
+        for _ in range(self.livecycle):
             self.cross_over()
             self.update_best_fitness()
             self.mutation()
@@ -34,21 +49,21 @@ class GeniticAlgoithm:
             self.selection()
 
     def update_best_fitness(self: object) -> None:
-        self.best_fitness = max(self.__fitness)
-        self.best_chromosome = self.__chromosome[self.__fitness.index(self.best_fitness)]
+        self.best_fitness = max(self.fitness)
+        self.best_chromosome = self.chromosome[self.fitness.index(self.best_fitness)]
     
     def cross_over(self: object) -> None:
-        self.__chromosome.extend([self.generate_chromosome() for _ in range(self.__population_size)])
-        self.__fitness.extend([self.cal_fitness(chromosome) for chromosome in self.__chromosome[self.__population_size:]])
+        self.chromosome.extend([self.generate_chromosome() for _ in range(self.population_size)])
+        self.fitness.extend([self.cal_fitness(chromosome) for chromosome in self.chromosome[self.population_size:]])
 
     def mutation(self: object) -> None:
         pass
 
     def selection(self: object) -> None:
-        self.__chromosome = [chromosome for _, chromosome in sorted(zip(self.__fitness, self.__chromosome), reverse=True)]
-        self.__fitness = sorted(self.__fitness, reverse=True)
-        self.__chromosome = self.__chromosome[:self.__population_size]
-        self.__fitness = self.__fitness[:self.__population_size]
+        self.chromosome = [chromosome for _, chromosome in sorted(zip(self.fitness, self.chromosome), reverse=True)]
+        self.fitness = sorted(self.fitness, reverse=True)
+        self.chromosome = self.chromosome[:self.population_size]
+        self.fitness = self.fitness[:self.population_size]
 
     def output_chromosome(self: object) -> None:
         fileName = f'chromosome_{self.best_fitness}_{time.strftime("%m%d%H%M", time.localtime())}.json'
@@ -74,11 +89,11 @@ class GeniticAlgoithm:
             }
         ]        
         '''
-        chromosome = self.__gene_list.copy()
+        chromosome = self.gene_list.copy()
         for course in chromosome:
-            course['week'] = random.randint(self.__START_WEEK, self.__END_WEEK)
-            course['session'] = random.choice(self.__session_list)
-            course['classroom'] = random.choice(self.__class_list)
+            course['week'] = random.randint(self.START_WEEK, self.END_WEEK)
+            course['session'] = random.choice(self.session_list)
+            course['classroom'] = random.choice(self.class_list)
         return chromosome
     
     def cal_fitness(self: object, chromosome: list[dict]) -> int:
@@ -127,14 +142,14 @@ class GeniticAlgoithm:
             for teacher in gene['teacher_list']:
                 if teacher not in teacher_class_week:
                     teacher_class_week[teacher] = {}
-                    for week in range(self.__START_WEEK, self.__END_WEEK + 1):
+                    for week in range(self.START_WEEK, self.END_WEEK + 1):
                         teacher_class_week[teacher][week] = {}
                 teacher_session = teacher_class_week[teacher][gene['week']].get(gene['session'], 0)
                 teacher_class_week[teacher][gene['week']][gene['session']] = teacher_session + 1
             #建立course_week的邏輯 沒特別check
             if gene['course_id'] not in course_week:
                 course_week[gene['course_id']] = {}
-                for week in range(self.__START_WEEK, self.__END_WEEK + 1):
+                for week in range(self.START_WEEK, self.END_WEEK + 1):
                     course_week[gene['course_id']][str(week)] = {}
             course_session = course_week[gene['course_id']][str(gene['week'])].get(gene['session'], 0)
             course_week[gene['course_id']][str(gene['week'])][gene['session']] = course_session + 1
@@ -143,7 +158,7 @@ class GeniticAlgoithm:
             # penalty rule #4
             fitness -= 100 if gene['session'] == '20' else 0
             # penalty rule #2
-            fitness -= 30 if self.__session_list.index(gene['session']) > self.__session_list.index('07') else 0 
+            fitness -= 30 if self.session_list.index(gene['session']) > self.session_list.index('07') else 0 
 
         for teacher in teacher_class_week:
             overtime = False
@@ -168,11 +183,11 @@ class GeniticAlgoithm:
 
                 if len(course_week[course][week]) == 3:
                     # print('333', course_week[course][week])
-                    session_index = [self.__session_list.index(_) for _ in course_week[course][week]]
+                    session_index = [self.session_list.index(_) for _ in course_week[course][week]]
                     min_index = min(session_index)
                     is_3_session = True if sum(session_index) - 3 * min_index == 3 else False
-                    session_start_25 |= True if min_index == self.__session_list.index('02') \
-                                            or min_index == self.__session_list.index('05') else False
+                    session_start_25 |= True if min_index == self.session_list.index('02') \
+                                            or min_index == self.session_list.index('05') else False
                     '''
                     04 20 05 vaild?
                     '''
--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from multiprocessing import Process, Manager
 def run_ga(all_ga: list[int]) -> None:
     FILE_NAME = 'test_gene_washed.json'
     # FILE_NAME = 'little_gene.json'
-    ga = GeniticAlgoithm(FILE_NAME, 'class.json', live=100, popu=1000)
+    GeniticAlgoithm.init(FILE_NAME, 'class.json', live=100, popu=100)
+    ga = GeniticAlgoithm()
     all_ga.append(ga)
 
 def main() -> None:
 
-    loops = 10
+    loops = 1
 
     with Manager() as manager:
         all_ga = manager.list()
@@ -24,7 +25,7 @@ def main() -> None:
     all_fitness = [_.best_fitness for _ in all_ga]
     all_fitness, all_ga = zip(*sorted(zip(all_fitness, all_ga), reverse=True))
     print(all_fitness[:10])
-    all_ga[0].output_chromosome()
+    # all_ga[0].output_chromosome()
 
 if __name__ == '__main__':
     main()
