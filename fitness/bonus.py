4bb5eaad8979d01017fab44a5d5998709661793d
dong3 <ac@dong3.me>
2023-10-11 20:47:10 +0800

    fix: bonus-rule_1 calculate logic the rule_1 bonus condition need continues 3 lesson

--- fitness/bonus.py
+++ fitness/bonus.py
@@ -4,5 +4,15 @@ class Bonus:
     def __init__(self, session_list: list[str]):
         self.session_list = session_list
 
-    def rule_1(self, course_week_total: int, is_3_session: bool, session_start_25: bool) -> int:
-        return 30 * course_week_total if is_3_session and session_start_25 else 0
+    def rule_1(self, course_time_dict: dict) -> int:
+        fitness = 0
+        for course in course_time_dict:
+            #course_time_set = {(classroom, week)}
+            course_time_set = set(tuple(val[:2]) for val in course_time_dict[course])
+            if len(course_time_set) == 1:
+                course_session_list = [val[2] for val in course_time_dict[course]]
+                course_session_idx = [self.session_list.index(val) for val in course_session_list]
+                course_session_idx.sort()
+                if course_session_idx[0] == 1 or course_session_idx[0] == 5:
+                    fitness += 100 * len(course_time_dict[course])
+        return fitness
