76dc2399e3d90648a02eba4309203f381c54cb9c
dong3 <ac@dong3.me>
2023-10-12 18:59:57 +0800

    feat: add fitness_weight and change fitness calculate method

--- fitness/__init__.py
+++ fitness/__init__.py
@@ -9,60 +9,26 @@ class Fitness():
     def __init__(self):
         self.penalty = Penalty(self.session_list)
         self.bonus = Bonus(self.session_list)
+        with open('fitness/fitness_weight.cfg', 'r') as f:
+            self.fitness_weight =  {line.split('=')[0].strip(): int(line.split('=')[1]) for line in f.readlines()}
 
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         return self.compute_fitness(*args, **kwds)
 
-    def compute_fitness(self, chromosome: list[dict]) -> int:
-        teacher_class_week = {}
-        course_week = {}
-        fitness = [dict(), dict()] # [penalty, bonus]
+    def compute_fitness(self, chromosome: list[dict]) -> dict:
+        fitness = {
+            'penalty': dict(),
+            'bonus': dict()
+        }
 
-        for gene in chromosome:
-            for teacher in gene['teacher_list']:
-                if teacher not in teacher_class_week:
-                    teacher_class_week[teacher] = {}
-                    for week in range(self.START_WEEK, self.END_WEEK + 1):
-                        teacher_class_week[teacher][week] = {}
-                teacher_session = teacher_class_week[teacher][gene['week']].get(gene['session'], 0)
-                teacher_class_week[teacher][gene['week']][gene['session']] = teacher_session + 1
+        fitness['bonus']['start_on_2_5'] = self.bonus.start_on_2_5(chromosome)
+        fitness['penalty']['exceed_time'] = self.penalty.exceed_time(chromosome)
+        fitness['penalty']['over_07'] = self.penalty.over_07(chromosome)
+        fitness['penalty']['working_overtime'] = self.penalty.working_overtime(chromosome)
 
-            if gene['course_id'] not in course_week:
-                course_week[gene['course_id']] = {}
-                for week in range(self.START_WEEK, self.END_WEEK + 1):
-                    course_week[gene['course_id']][str(week)] = {}
-            course_session = course_week[gene['course_id']][str(gene['week'])].get(gene['session'], 0)
-            course_week[gene['course_id']][str(gene['week'])][gene['session']] = course_session + 1
-
-            fitness[0][2] = self.penalty.rule_2(gene)
-            fitness[0][4] = self.penalty.rule_4(gene)
-
-        for teacher in teacher_class_week:
-            fitness[0][3] = self.penalty.rule_3(teacher_class_week[teacher])
-            fitness[0][6] = self.penalty.rule_6(teacher_class_week[teacher])
-
-        for course in course_week:
-            # print(course_week)
-            lst = self.evaluate_course_week(course_week[course])
-            fitness[0][1] = lst[0]
-            fitness[1][1] = lst[1]
+        for subdict in fitness:
+            for key in fitness[subdict]:
+                fitness[subdict][key] *= self.fitness_weight[key]
 
         return fitness
 
-    def evaluate_course_week(self, course_week: dict) -> list[int]:
-        course_week_total = 0
-        is_3_session = False
-        session_start_25 = False
-
-        for week in course_week:
-            course_week_total += sum(course_week[week].values())
-
-            if len(course_week[week]) == 3:
-                session_index = [self.session_list.index(_) for _ in course_week[week]]
-                min_index = min(session_index)
-                is_3_session = sum(session_index) - 3 * min_index == 3
-                session_start_25 |= min_index == self.session_list.index('02') or min_index == self.session_list.index('05')
-
-        #[penalty, bonus]
-        fitness_list = [self.penalty.rule_1(course_week_total, is_3_session), self.bonus.rule_1(course_week_total, is_3_session, session_start_25)]
-        return fitness_list
